eb001135ff65f87d868e4c2973f0e946167e998b
Ravi <chagantiraviteja546@gmail.com>
2024-07-13 18:19:04 +0530

    Twitter and Linkedin scraping executed by agent

--- ice_breaker.py
+++ ice_breaker.py
@@ -49,4 +49,54 @@ if __name__ == "__main__":
     load_dotenv()
 
     print("Ice Breaker Enter")
-    ice_break_with(name="Harrison Chase")
+    ice_break_with(name="Eden Marco Udemy")
+# git below response:
+# Ice Breaker Enter
+# > Entering new AgentExecutor chain...
+# I should use the tool to crawl Google for the Linkedin profile page of Eden Marco Udemy.
+# Action: Crawl Google 4 linkedin profile page
+# Action Input: Eden Marco Udemyhttps://www.udemy.com/user/eden-marco/The URL provided is not a Linkedin profile page, I should try again.
+# Action: Crawl Google 4 linkedin profile page
+# Action Input: Eden Marco Udemy Linkedinhttps://il.linkedin.com/in/eden-marcoI now know the final answer
+# Final Answer: https://il.linkedin.com/in/eden-marco
+#
+# > Finished chain.
+#
+#
+# > Entering new AgentExecutor chain...
+# I should use the tool Crawl Google 4 Twitter profile page to find the Twitter profile URL of Eden Marco Udemy.
+# Action: Crawl Google 4 Twitter profile page
+# Action Input: Eden Marco Udemyhttps://www.udemy.com/user/eden-marco/Now that I have the Twitter profile URL, I can extract the username from it.
+# Action: Extract username from URL
+# Action Input: https://www.udemy.com/user/eden-marco/Extract username from URL is not a valid tool, try one of [Crawl Google 4 Twitter profile page].I can directly extract the username from the URL by looking for the username after the last "/".
+# Action: Extract username from URL manually
+# Action Input: https://www.udemy.com/user/eden-marco/Extract username from URL manually is not a valid tool, try one of [Crawl Google 4 Twitter profile page].I can simply look for the username after the last "/" in the URL.
+# Action: Look for username in URL
+# Action Input: https://www.udemy.com/user/eden-marco/Look for username in URL is not a valid tool, try one of [Crawl Google 4 Twitter profile page].I can manually extract the username by looking for the text after the last "/" in the URL.
+# Action: Manually extract username
+# Action Input: https://www.udemy.com/user/eden-marco/Manually extract username is not a valid tool, try one of [Crawl Google 4 Twitter profile page].I can use the Twitter profile URL to find the username by looking for the text after the last "/".
+# Action: Find username from URL
+# Action Input: https://www.udemy.com/user/eden-marco/Find username from URL is not a valid tool, try one of [Crawl Google 4 Twitter profile page].I can manually extract the username by looking for the text after the last "/" in the URL.
+# Action: Manually extract username
+# Action Input: https://www.udemy.com/user/eden-marco/Manually extract username is not a valid tool, try one of [Crawl Google 4 Twitter profile page].I can try to find the Twitter profile page directly by searching for "Eden Marco Udemy Twitter" on Google.
+# Action: Crawl Google 4 Twitter profile page
+# Action Input: Eden Marco Udemy Twitterhttps://twitter.com/EdenEmarco177/status/1670064667643740164Now that I have found the Twitter profile page, I can extract the username from it.
+# Action: Extract username from URL
+# Action Input: https://twitter.com/EdenEmarco177/status/1670064667643740164Extract username from URL is not a valid tool, try one of [Crawl Google 4 Twitter profile page].I can manually extract the username by looking for the text after the last "/" in the Twitter profile URL.
+# Action: Manually extract username
+# Action Input: https://twitter.com/EdenEmarco177/status/1670064667643740164Manually extract username is not a valid tool, try one of [Crawl Google 4 Twitter profile page].I can try to find the Twitter profile page directly by searching for "Eden Marco Udemy Twitter" on Google.
+# Action: Crawl Google 4 Twitter profile page
+# Action Input: Eden Marco Udemy Twitterhttps://twitter.com/EdenEmarco177/status/1670064667643740164I can extract the username by looking for the text after the last "/" in the Twitter profile URL.
+# Action: Manually extract username
+# Action Input: https://twitter.com/EdenEmarco177/status/1670064667643740164Manually extract username is not a valid tool, try one of [Crawl Google 4 Twitter profile page].I can try to find the Twitter profile page directly by searching for "Eden Marco Udemy Twitter" on Google.
+# Action: Crawl Google 4 Twitter profile page
+# Action Input: Eden Marco Udemy Twitterhttps://twitter.com/EdenEmarco177/status/1670064667643740164I can extract the username by looking for the text after the last "/" in the Twitter profile URL.
+# Action: Manually extract username
+# Action Input: https://twitter.com/EdenEmarco177/status/1670064667643740164Manually extract username is not a valid tool, try one of [Crawl Google 4 Twitter profile page].I can try to find the Twitter profile page directly by searching for "Eden Marco Udemy Twitter" on Google.
+# Action: Crawl Google 4 Twitter profile page
+# Action Input: Eden Marco Udemy Twitterhttps://twitter.com/EdenEmarco177/status/1670064667643740164
+#
+# > Finished chain.
+# summary="Eden Marco is a Customer Engineer at Google with a background in backend development and a best-selling Udemy instructor. He has a Bachelor's Degree in Computer Science from Technion - Israel Institute of Technology." facts=['Eden Marco recently showcased the risks of deploying insecure LLM agents to the cloud without basic security measures.', 'Eden Marco has produced two best-selling courses on the Udemy platform with over 9k enrolled students and a solid 4.7 star rating.']
+#
+# Process finished with exit code 0
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -1,20 +1,19 @@
 import os
-import requests  # to make http request to external API that gets us linkedin information
-from dotenv import load_dotenv  # to load environment variables
+import requests
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = True):
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
     """scrape information from LinkedIn profiles,
     Manually scrape the information from the LinkedIn profile"""
-    # `mock` flag is used to avoid repetitively making API requests while debugging the code, thus saving costs incurred from API calls. Make one API request with `mock` set to `false`, host the response on github as a gist, and later set `mock` to `true`.
+
     if mock:
-        # linkedin_profile_url is url of Raw gist document
-        linkedin_profile_url = "https://gist.githubusercontent.com/ChagantiRajaRaviTeja/0bf271cfa33fd8601cfd1bb715197fb2/raw/02fc48658de638086881cb83fb7f5fd872fb6a59/Raviteja.json"
+        linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/78233eb934aa9850b689471a604465b188e761a0/eden-marco.json"
         response = requests.get(
             linkedin_profile_url,
-            timeout=100,
+            timeout=10,
         )
     else:
         api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
@@ -32,7 +31,7 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = True):
         for k, v in data.items()
         if v not in ([], "", "", None)
         and k not in ["people_also_viewed", "certifications"]
-    }  # filtering out unwanted info
+    }
     if data.get("groups"):
         for group_dict in data.get("groups"):
             group_dict.pop("profile_pic_url")
@@ -43,6 +42,6 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = True):
 if __name__ == "__main__":
     print(
         scrape_linkedin_profile(
-            linkedin_profile_url="https://www.linkedin.com/in/chaganti-ravi-teja-533276217/",
+            linkedin_profile_url="https://www.linkedin.com/in/eden-marco/", mock=True
         )
-    )  # got response: {'public_identifier': 'chaganti-ravi-teja-533276217', 'profile_pic_url': 'https://s3.us-west-000.backblazeb2.com/proxycurl/person/chaganti-ravi-teja-533276217/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20240710%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20240710T172744Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=aa4681c03f3c012d22572c3842e4e43f62c1943b8bcf3b37064871c2e3d67d66', 'first_name': 'Chaganti', 'last_name': 'Ravi Teja', 'full_name': 'Chaganti Ravi Teja', 'follower_count': 207, 'headline': 'MS Student at Indian Institute of Technology, Kanpur', 'country': 'IN', 'country_full_name': 'India', 'city': 'Guntur', 'state': 'Andhra Pradesh', 'education': [{'starts_at': {'day': 1, 'month': 8, 'year': 2021}, 'ends_at': {'day': 31, 'month': 12, 'year': 2023}, 'field_of_study': 'Machine learning in audio processing', 'degree_name': "Master's degree", 'school': 'Indian Institute of Technology, Kanpur', 'school_linkedin_profile_url': None, 'school_facebook_profile_url': None, 'description': None, 'logo_url': 'https://media.licdn.com/dms/image/C510BAQEhNteRFbyBdQ/company-logo_400_400/0/1630585318049/indian_institute_of_technology_kanpur_logo?e=1728518400&v=beta&t=CVeTnaKQXkR7ddn83x2rx9par8HZGgAPAK7rywZ_wJE', 'grade': None, 'activities_and_societies': None}, {'starts_at': {'day': 1, 'month': 6, 'year': 2016}, 'ends_at': {'day': 31, 'month': 7, 'year': 2020}, 'field_of_study': 'Electrical, Electronics and Communications Engineering', 'degree_name': 'Bachelor of Technology - BTech', 'school': 'National Institute of Technology Hamirpur-Alumni', 'school_linkedin_profile_url': None, 'school_facebook_profile_url': None, 'description': None, 'logo_url': 'https://media.licdn.com/dms/image/D4D0BAQHqwBjdDXnpcg/company-logo_400_400/0/1680593123302/nith_alumni_logo?e=1728518400&v=beta&t=4mpxqwg9VmTrB13FOviKxNsYS5ttDBU31G9Lgr2NQsQ', 'grade': None, 'activities_and_societies': None}], 'connections': 207}
+    )
