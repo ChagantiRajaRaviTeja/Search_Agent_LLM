ce34206c54e9066c5022496341ea5e91531b8157
Ravi <chagantiraviteja546@gmail.com>
2024-07-13 13:23:58 +0530

    Search-Agent impplemented using Tavily API

--- tools/tools.py
+++ tools/tools.py
@@ -1,8 +1,17 @@
+import os
+
 from langchain_community.tools.tavily_search import TavilySearchResults
+from dotenv import load_dotenv
 
 
 def get_profile_url_tavily(name: str):
     """Searches for Linkedin or Twitter Profile Page."""
     search = TavilySearchResults()
     res = search.run(f"{name}")
+    print(res)
     return res[0]["url"]
+# if __name__=="__main__":
+#     load_dotenv()
+#     print(os.environ['TAVILY_API_KEY'])
+#     result = get_profile_url_tavily(name = "Linkedin Eden Marco")
+#     print(result)
